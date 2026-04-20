72be162c4ea47e501bbdccec63c4321a15b79406
Smit0809 <sumittathod@gmail.com>
2026-04-20 14:54:11 +0530

    Front-end UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: ProductsComponent },
+
+];
